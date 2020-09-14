e92a89f639f263df82989054af2a14a8e05e3577
Pete Hodgson <git@thepete.net>
2020-09-13 17:03:01 -0700

    start.spring.io generated template

---
+++ src/main/java/io/fauxstore/deals/DealsSvcApplication.java
@@ -0,0 +1,13 @@
+package io.fauxstore.deals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealsSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealsSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/fauxstore/deals/DealsSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package io.fauxstore.deals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealsSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
