bf7b1885f9a58b093be94cd69ec767f13cabbebc
Pete Hodgson <git@thepete.net>
2020-09-18 16:27:53 -0700

    add honeycomb

--- src/main/java/db/migration/V2__PopulateDealsTable.java
+++ src/main/java/db/migration/V2__PopulateDealsTable.java
@@ -2,6 +2,7 @@ package db.migration;
 
 import io.fauxstore.deals.Deal;
 import io.fauxstore.deals.db.DealsRepository;
+import io.honeycomb.beeline.tracing.Beeline;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 import org.jdbi.v3.core.Jdbi;
@@ -15,7 +16,8 @@ public class V2__PopulateDealsTable extends BaseJavaMigration {
         final Jdbi jdbi = Jdbi.create(context.getConnection());
         jdbi.installPlugin(new SqlObjectPlugin());
 
-        final DealsRepository dealsRepository = new DealsRepository(jdbi);
+        final Beeline nullBeeline = null;
+        final DealsRepository dealsRepository = new DealsRepository(jdbi, nullBeeline);
 
         IntStream.rangeClosed(1, 100).forEach(x -> dealsRepository.insertDeal(Deal.fakeDeal()));
     }
--- src/main/java/io/fauxstore/deals/db/DealsRepository.java
+++ src/main/java/io/fauxstore/deals/db/DealsRepository.java
@@ -1,6 +1,9 @@
 package io.fauxstore.deals.db;
 
 import io.fauxstore.deals.Deal;
+import io.honeycomb.beeline.spring.beans.aspects.ChildSpan;
+import io.honeycomb.beeline.spring.beans.aspects.SpanField;
+import io.honeycomb.beeline.tracing.Beeline;
 import org.jdbi.v3.core.Jdbi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,18 +13,22 @@ import java.util.List;
 @Service
 public class DealsRepository {
     private final Jdbi jdbi;
+    private final Beeline beeline;
 
 
     @Autowired
-    public DealsRepository(Jdbi jdbi) {
+    public DealsRepository(Jdbi jdbi, Beeline beeline) {
         this.jdbi = jdbi;
+        this.beeline = beeline;
     }
 
+    @ChildSpan("insertDeal")
     public void insertDeal(Deal deal){
         jdbi.useExtension(DealsDao.class, dao-> dao.insert(deal));
     }
 
-    public List<Deal> selectSomeDeals(Integer numberOfDeals) {
+    @ChildSpan("selectSomeDeals")
+    public List<Deal> selectSomeDeals(@SpanField Integer numberOfDeals) {
         return jdbi.withExtension(DealsDao.class, dao-> dao.selectN(numberOfDeals));
     }
 }
